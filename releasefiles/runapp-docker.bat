b656a8d3fac83e5cd60e738064242988c79ce751
Sola <solao@novi.local>
2022-02-25 11:39:11 +0000

    load metrics from external directory:7

--- releasefiles/runapp-docker.bat
+++ releasefiles/runapp-docker.bat
@@ -1,6 +1,8 @@
 @echo off
 
-set workdir=%cd%
+REM full pathname to directory containing metrics csv files
 set metricsdir=%1%
+
+set workdir=%cd%
 
 docker run -p 4040:4040 -v ${workdir}:/config -v %metricsdir%:/iqmetrics ghcr.io/sonatype-nexus-community/nexusiq-successmetrics:@APPVER@
--- releasefiles/runapp-docker.sh
+++ releasefiles/runapp-docker.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
 
-workdir=$(pwd)
+# full pathname to directory containing metrics csv files
 metricsdir=${1}
+
+workdir=$(pwd)
 
 docker run -p 4040:4040 -v "${workdir}":/config -v "${metricsdir}":/iqmetrics ghcr.io/sonatype-nexus-community/nexusiq-successmetrics:@APPVER@
