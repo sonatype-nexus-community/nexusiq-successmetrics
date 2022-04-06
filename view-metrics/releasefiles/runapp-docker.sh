852cb7fd8383d567dc35ab036bdba2d2af155c14
Richard Panman <rpanman@sonatype.com>
2022-04-06 14:45:58 +0100

    Automatically delete Docker containers after run

--- view-metrics/releasefiles/runapp-docker.sh
+++ view-metrics/releasefiles/runapp-docker.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-docker run -p 4040:4040 -v "$PWD"/config:/config -v "$PWD"/../iqmetrics/:/iqmetrics ghcr.io/sonatype-nexus-community/nexusiq-successmetrics-view-metrics:@APPVER@ "$@"
+docker run --rm -p 4040:4040 -v "$PWD"/config:/config -v "$PWD"/../iqmetrics/:/iqmetrics ghcr.io/sonatype-nexus-community/nexusiq-successmetrics-view-metrics:@APPVER@ "$@"
