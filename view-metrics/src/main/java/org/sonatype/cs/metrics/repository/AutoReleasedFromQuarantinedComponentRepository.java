da9b7055694f61f99a4aca76e0f9acbf561c36bd
Richard Panman <rpanman@sonatype.com>
2022-05-10 13:36:43 +0100

    Additional firewall tables:
    1. quarantined components by format and threat level
    2. quarantined components by threat level by month

--- view-metrics/src/main/java/org/sonatype/cs/metrics/repository/AutoReleasedFromQuarantinedComponentRepository.java
+++ view-metrics/src/main/java/org/sonatype/cs/metrics/repository/AutoReleasedFromQuarantinedComponentRepository.java
@@ -2,8 +2,14 @@ package org.sonatype.cs.metrics.repository;
 
 import org.sonatype.cs.metrics.model.AutoReleasedFromQuarantineComponent;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface AutoReleasedFromQuarantinedComponentRepository
-        extends JpaRepository<AutoReleasedFromQuarantineComponent, Long> {}
+        extends JpaRepository<AutoReleasedFromQuarantineComponent, Long> {
+    @Query("SELECT DISTINCT displayName FROM AutoReleasedFromQuarantineComponent")
+    List<String> findDistinctDisplayName();
+}
